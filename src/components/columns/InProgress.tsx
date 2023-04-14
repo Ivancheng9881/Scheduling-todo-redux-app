b131c189c8815fbdcd6c2e9dc28b6e71c3fd211d
Ivan Cheng <ivancheng@Ivans-MacBook-Pro-2.local>
2023-04-14 16:44:09 +0800

    scheduling stuff

--- src/components/columns/InProgress.tsx
+++ src/components/columns/InProgress.tsx
@@ -1,18 +1,58 @@
-import { useSelector } from 'react-redux';
-import { StoreState } from '../../redux/store';
-import { inProgressSlice } from '../../redux/slice/inProgress';
-import ScheduleLayout from '../ScheduleLayout';
-import { Typography } from '@mui/material';
-import { IModel } from '../../types';
+import { useSelector } from "react-redux";
+import { StoreState } from "../../redux/store";
+import { inProgressSlice } from "../../redux/slice/inProgress";
+import ScheduleLayout from "../ScheduleLayout";
+import { Typography } from "@mui/material";
+import { IModel } from "../../types";
 
 export function InProgressColumn() {
   const { todo } = useSelector((state: StoreState) => state);
   const scheduledTasks = (todoArr: IModel[]): IModel[] => {
-    return todoArr.filter((e) => e.isScheduled === true);
+    return todoArr;
   };
 
+  const separateTasks = (scheduledTasks: IModel[]) => {
+    let randomArr: IModel[][] = [];
+    let miniArr: IModel[] = [];
+    let counter = 0;
+    for (let i = 0; i < scheduledTasks.length; i++) {
+      console.log(scheduledTasks[i].duration);
+      if (scheduledTasks[i].duration + counter < 8) {
+        counter = counter + scheduledTasks[i].duration;
+        miniArr.push(scheduledTasks[i]);
+        console.log({ miniArr });
+      } else if (scheduledTasks[i].duration + counter > 8) {
+        randomArr.push(miniArr);
+        miniArr = [];
+        miniArr.push(scheduledTasks[i]);
+        counter = 0;
+        counter = counter + scheduledTasks[i].duration;
+      } else {
+        miniArr.push(scheduledTasks[i]);
+        randomArr.push(miniArr);
+        counter = 0;
+        miniArr = [];
+      }
+
+      if (i === (scheduledTasks.length - 1)) {
+        randomArr.push(miniArr);
+      }
+    }
+    return randomArr;
+  };
+
+  console.log(separateTasks(todo));
+
   const {
-    actions: { completeStatus, remove, add, updateTextShowed, increment, decrement, schedule },
+    actions: {
+      completeStatus,
+      remove,
+      add,
+      updateTextShowed,
+      increment,
+      decrement,
+      schedule,
+    },
   } = inProgressSlice;
 
   return (
